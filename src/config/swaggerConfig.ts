22a790f9be2adbcf0a179f256273fcefd574ce95
Miguel <miguelenrique1805@gmail.com>
2023-02-02 01:34:40 -0500

    added path

--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -13,6 +13,10 @@ export const options = {
         url: `http://localhost:5000`,
         description: 'Local server development',
       },
+      {
+        url: `https://canchitas-backend-production.up.railway.app`,
+        description: 'Local server development',
+      },
     ],
   },
   apis: ['src/docs/**/*.yaml', 'src/auth/swagger/*.yaml'],
